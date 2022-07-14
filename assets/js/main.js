2cd14c68e194848a14c7b3ffb7a8318822f42ce3
KhoaPC <luongvankhoapc@gmail.com>
2022-07-14 10:34:03 +0700

    22Jul14 UPDATE drag drop output title #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,49 @@ btnUrl.onclick = function () {
     } catch { }
 } // btnUrl.onclick
 
+
+//Make the DIV element draggagle:
+function dragElement(element) {
+    var pos1 = 0, pos2 = 0;
+    if (containerApp.querySelector(`#${element.id}`)) {
+        /* if present, the header is where you move the DIV from:*/
+        containerApp.querySelector(`#${element.id}`).onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        element.onmousedown = dragMouseDown;
+    }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos2 = e.clientY;
+        containerApp.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        containerApp.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos2 - e.clientY;
+        pos2 = e.clientY;
+        element.style.top = element.offsetTop - pos1;
+
+        if (element.offsetTop - pos1 < 0) {
+            element.style.top = 0;
+        } else if (element.offsetTop - pos1 > 360) {
+            element.style.top = 360;
+        }
+    }
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        containerApp.onmouseup = null;
+        containerApp.onmousemove = null;
+    }
+} // dragElement
+dragElement(containerApp.querySelector("#drag-drop"));
+
+
 function encodeImageFileAsURL(element) {
     const file = element.files[0];
     const reader = new FileReader();
@@ -58,51 +101,7 @@ btnSaveImg.onclick = () => {
     html2canvas(image).then(function (canvas) {
         let anchor = document.createElement('a');
         anchor.href = canvas.toDataURL('image/png');
-        anchor.download = 'IMAGE.PNG';
+        anchor.download = 'image.PNG';
         anchor.click();
     });
 } // btnSaveImg.onclick
-
-//Make the DIV element draggagle:
-dragElement(containerApp.querySelector("#chay"));
-
-function dragElement(element) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(element.id)) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById(element.id).onmousedown = dragMouseDown;
-    } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        element.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        // pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        // pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        // pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        element.style.top = (element.offsetTop - pos2) + "px";
-        // element.style.left = (element.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
